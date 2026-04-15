ab4025ec9a3642348be316b4cd9dcc19d3ed34a2
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-04-15 15:06:41 +0400

    Day2 :Task 5 – Sum of Even and Odd Numbers

--- Solution02/project1/Program.cs
+++ Solution02/project1/Program.cs
@@ -170,38 +170,67 @@ namespace project1
 
 
             //Task 4 – Season Detector with Month Validation
-            Console.WriteLine("Enter Month number: ");
-            int month = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter Month number: ");
+            //int month = Convert.ToInt32(Console.ReadLine());
 
-            switch (month)
+            //switch (month)
+            //{
+            //    case 12:
+            //    case 1:
+            //    case 2:
+            //        Console.WriteLine("Winter");
+            //        break;
+
+            //    case 3:
+            //    case 4:
+            //    case 5:
+            //        Console.WriteLine("Spring");
+            //        break;
+
+            //    case 6:
+            //    case 7:
+            //    case 8:
+            //        Console.WriteLine("Summer");
+            //        break;
+
+            //    case 9:
+            //    case 10:
+            //    case 11:
+            //        Console.WriteLine("Autumn");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Invalid month number");
+            //        break;
+            //}
+
+
+            //Task 5 – Sum of Even and Odd Numbers
+            Console.WriteLine("Enter a positive integer: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n <= 0)
             {
-                case 12:
-                case 1:
-                case 2:
-                    Console.WriteLine("Winter");
-                    break;
+                Console.WriteLine("Please enter a positive integer");
+            }
+            else
+            {
+                int evenSum = 0;
+                int oddSum = 0;
 
-                case 3:
-                case 4:
-                case 5:
-                    Console.WriteLine("Spring");
-                    break;
-
-                case 6:
-                case 7:
-                case 8:
-                    Console.WriteLine("Summer");
-                    break;
-
-                case 9:
-                case 10:
-                case 11:
-                    Console.WriteLine("Autumn");
-                    break;
-
-                default:
-                    Console.WriteLine("Invalid month number");
-                    break;
+                for (int i = 1; i <= n; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        evenSum += i;
+                    }
+                    else
+                    {
+                        oddSum += i;
+                    }
+                }
+                Console.WriteLine($"Sum of even number ={evenSum}");
+                Console.WriteLine($"Sum of odd number ={oddSum}");
             }
 
         }
