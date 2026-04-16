5ff3ec04784b2c9ecbeba7ff826fa8e59c9c64ed
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-04-16 09:28:57 +0400

    Day 3: for loop

---
+++ Solution03/project1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           //use for to take num from user
+            for (int i = 1; i < 5; i++)
+            {
+
+                Console.WriteLine("Enter Number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("number  "+ num);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
