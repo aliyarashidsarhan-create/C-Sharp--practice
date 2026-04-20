b5c663e9a247f31bff369d453a609fe36609a06c
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-04-20 09:15:24 +0400

    Day5:create array

---
+++ Solution05/project01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //aray of 4 number
+            int[] Numbers = new int[4];
+
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Console.WriteLine("Enter any number:");
+                Numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("***************************");
+            foreach (int num in Numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
