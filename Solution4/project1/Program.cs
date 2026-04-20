5e91e9f1f6727e8fe6db3e8586a824f7f84fe2c4
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-04-20 08:37:06 +0400

    Day4:Error type

---
+++ Solution4/project1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+
+        }
+    }
+}
