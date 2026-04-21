5ec297e3969154c2d43b647acd7b8c3c85892baa
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-04-21 11:45:16 +0400

    Day6: array

---
+++ Solution06/project01/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region array 
+            //Console.Write("Enter number of rows: ");
+            //int rows=Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Enter number of colums: ");
+            //int cols=Convert.ToInt32(Console.ReadLine());
+
+            //int [,] numbers = new int[rows, cols];
+
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    for(int j = 0;j< cols;j++)
+            //    {
+            //        Console.Write($"Enter value for [{i},{j}]: ");
+            //        numbers[i,j] = Convert.ToInt32(Console.ReadLine());
+            //    }
+            //}
+
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    for (int j = 0; j < cols; j++)
+            //    {
+            //        Console.Write($"The  value of [{i},{j}]:is {numbers[i, j]} ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+
+            #endregion
+
+
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
