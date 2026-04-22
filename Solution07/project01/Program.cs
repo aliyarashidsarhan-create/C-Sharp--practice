a3548e0a109536ac001405616d861921c95054ed
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-04-22 09:14:54 +0400

    Day7:Create and read file

---
+++ Solution07/project01/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            #region Create file
+
+            // File.Create("file1.txt");
+
+           // string[] lines1 = { "Sara", "Ali", "huda", "Salim" };
+
+          //  File.AppendAllLines("file1.txt", lines1);
+
+            string myfile = File.ReadAllText("file1.txt");
+            Console.WriteLine(myfile);  
+
+
+            #endregion
+        }
+    }
+}
