03a994c70bb455e47cfc6f345f7465d4b2f34a92
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-05-03 13:43:21 +0400

    Day11:overloading

---
+++ Solution11/project01/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace project01
+{
+    internal class Program
+    {
+        // 2 numbers → SUM
+        int Calculate(int a, int b)
+        {
+            return a + b;
+        }
+        // 3 numbers → MULTIPLY
+        double Calculate(double a, double b)
+        {
+            return a * b ;
+        }
+        // 4 numbers → SUM
+        int Calculate(int a, int b, int c, int d)
+        {
+            return a + b + c + d;
+        }
+        static void Main(string[] args)
+        {
+
+
+            #region Task 1
+
+
+            Program obj = new Program(); 
+
+            Console.WriteLine(obj.Calculate(2, 3));
+            Console.WriteLine(obj.Calculate(2, 3));
+            Console.WriteLine(obj.Calculate(2, 3, 4, 5));
+
+
+
+
+            #endregion
+
+
+
+        }
+    }
+}
