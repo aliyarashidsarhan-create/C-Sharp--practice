2d27cf87881ad9f90b064726ad4abbc5d456e74c
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-05-05 09:41:28 +0400

    day13:use git and set

---
+++ Solution 13/project01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace project01
+{
+    internal class Program
+    {
+        class Car
+        {
+            public string color;
+            public int model_year {  get; set; }
+            private int temp;
+            public int Spead
+            {
+                get
+                {
+                    return temp;
+                }
+                set
+                {
+                    if (value >0)
+                        temp = value;
+                }
+            }
+            public void Drive()
+            {
+                Console.WriteLine("The car is driving...");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Car drive = new Car();
+            drive.Spead = 10;
+            Console.WriteLine( drive.Spead);
+           
+        }
+    }
+}
